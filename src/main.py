bc85b7fabe565020c68d0bbbb9018e7e1149857e
s0ham075 <sohamsawant075@gmail.com>
2025-03-22 01:32:01 +0530

    DeepSeek's goated

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from math import ceil
 def process_input_file(filepath: str):
     stats = {}
 
-    with open(filepath, "r") as f:
+    with open(filepath, "rb") as f:
         for row in f:
-            city, idli_str = row.strip().split(";")
+            city, idli_str = row.strip().split(b";")
             idli = float(idli_str)
             if city in stats:
                 stats_city = stats[city]
@@ -20,9 +20,15 @@ def process_input_file(filepath: str):
 
 
 def output_stats(stats: dict, filepath: str):
+    # with open(filepath, "w") as f:
+    #     for city, data in sorted(stats.items()):
+    #         f.write(f"{city}={data[0]}/{ceil((data[2]/data[3]) * 10) / 10}/{data[1]}\n")
+
     with open(filepath, "w") as f:
+        lines = []
         for city, data in sorted(stats.items()):
-            f.write(f"{city}={data[0]}/{ceil((data[2]/data[3]) * 10) / 10}/{data[1]}\n")
+            lines.append(f"{city.decode()}={data[0]}/{ceil((data[2]/data[3]) * 10) / 10}/{data[1]}\n")  
+        f.writelines(lines)  
 
 def main(input_file_name = "testcase.txt", output_file_name = "output.txt"):
     output_stats(process_input_file(input_file_name), output_file_name)
