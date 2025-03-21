8ef1c135803c9a27f24c88ad9f47181d00fbfad9
s0ham075 <sohamsawant075@gmail.com>
2025-03-22 00:25:11 +0530

    Winter is coming

--- src/main.py
+++ src/main.py
@@ -1,14 +1,31 @@
+from math import ceil
+
+
+def process_input_file(filepath: str):
+    stats = {}
+
+    with open(filepath, "r") as f:
+        for row in f:
+            city, idli_str = row.strip().split(";")
+            idli = float(idli_str)
+            if city in stats:
+                stats[city][0] = min(stats[city][0], idli)
+                stats[city][1] = max(stats[city][1], idli)
+                stats[city][2] += idli
+                stats[city][3] += 1
+            else:
+                stats[city] = [idli, idli, idli, 1]
+                
+    return stats
+
+
+def output_stats(stats: dict, filepath: str):
+    with open(filepath, "w") as f:
+        for city, data in sorted(stats.items()):
+            f.write(f"{city}={data[0]}/{ceil((data[2]/data[3]) * 10) / 10}/{data[1]}\n")
+
 def main(input_file_name = "testcase.txt", output_file_name = "output.txt"):
-    input_file = open(input_file_name, "r")
-    output_file = open(output_file_name, "w")
-
-    first_line = input_file.readline().strip()
-    first_line = first_line.split(";")
-
-    output_file.write(f"{first_line[0]}={first_line[1]}/{1}/{1}\n")
-
-    output_file.close()
-    input_file.close()
+    output_stats(process_input_file(input_file_name), output_file_name)
 
 if __name__ == "__main__":
     main()
